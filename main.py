29f0411eba5317d2709d5c871aef5f46ae274d0d
Kredis21354 <125788986+Kredis21354@users.noreply.github.com>
2023-03-04 20:04:40 +0000

    added dad

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ class Person:
   def __init__(self, name):
     self.name = name
 
+class Dad(Person):
+  name = "Ivan"
+  age = 49
+  
+  def __init__(self):
+    super().__init__("Dad")
+  
+
+
 
 
 
